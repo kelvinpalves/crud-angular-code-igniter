d4a0a7c87d3dfca851e202dc8df35331a45bec0d
Kelvin Alves <kelvinpalves@gmail.com>
2016-12-21 11:52:40 -0200

    Configurações do gulp para uglify com revisão do arquivo.

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var concat = require('gulp-concat');
+var del = require('del');
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var paths = require('./gulp.config.json');
+var rev = require('gulp-rev');
+var uglify = require('gulp-uglify');
+
+gulp.task('analisarCodigo', analisarCodigo);
+gulp.task('removerArquivos', removerArquivos);
+gulp.task('uglifyJs', uglifyJs);
+
+function analisarCodigo() {
+	gulp.src(paths.js)
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+}
+
+function removerArquivos() {
+	del(paths.build);
+}
+
+function uglifyJs() {
+	removerArquivos();
+	gulp.src([].concat(paths.uglify))
+		.pipe(concat('app.min.js'))
+		.pipe(uglify())
+		.pipe(rev())
+		.pipe(gulp.dest(paths.build));
+}
