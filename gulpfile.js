b8272ac4abc50f19f89730397dce6914f1bcb8fa
Kelvin Alves <kelvinpalves@gmail.com>
2016-12-22 13:41:18 -0200

    Minify CSS

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var concat = require('gulp-concat');
 var del = require('del');
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var minifyCss = require('gulp-minify-css');
 var paths = require('./gulp.config.json');
 var rev = require('gulp-rev');
 var uglify = require('gulp-uglify');
@@ -9,7 +10,8 @@ var uglify = require('gulp-uglify');
 gulp.task('analisarCodigo', analisarCodigo);
 gulp.task('default', padrao);
 gulp.task('removerArquivos', removerArquivos);
-gulp.task('uglifyJs', ['removerArquivos'], uglifyJs);
+gulp.task('minify', minify);
+gulp.task('compilar', ['removerArquivos', 'minify'], uglifyJs);
 
 function analisarCodigo() {
 	gulp.src(paths.js)
@@ -21,17 +23,25 @@ function removerArquivos() {
 	del(paths.build);
 }
 
+function minify() {
+	gulp.src([].concat(paths.minify))
+		.pipe(concat(paths.app.css))
+		.pipe(minifyCss())
+		.pipe(rev())
+		.pipe(gulp.dest(paths.build));
+}
+
 function uglifyJs() {
 	gulp.src([].concat(paths.uglify))
-		.pipe(concat('app.min.js'))
+		.pipe(concat(paths.app.js))
 		.pipe(uglify())
 		.pipe(rev())
 		.pipe(gulp.dest(paths.build));
 }
 
 function padrao() {
-	gulp.start('uglifyJs');
+	gulp.start('compilar');
 	gulp.watch(paths.js, function (evt) {
-		gulp.start('uglifyJs');
+		gulp.start('compilar');
 	});
 }
